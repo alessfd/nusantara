73c461e41ebad7889b8844d008cdf428d3117e8a
Jimmy <136186863+Jiira13@users.noreply.github.com>
2024-05-16 10:56:44 +0000

    missing style wisata

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,16 +1,33 @@
-import { Carousel } from '@material-tailwind/react';
-import CarouselDotsNavigation from './CarouselDotsNavigation';
+import { Carousel } from "@material-tailwind/react";
 
-const CarouselComponent = ({ images }) => (
-  <Carousel className="rounded-xl" navigation={CarouselDotsNavigation}>
-    {images.map((image, index) => (
-      <img
-        key={index}
-        src={image.src}
-        className="h-full w-full object-cover"
-      />
-    ))}
-  </Carousel>
-);
-
-export default CarouselComponent;
+export function CarouselCustomNavigation({ images }) {
+  return (
+    <Carousel
+      className="rounded-x"
+      loop={true}
+      autoplay={true}
+      autoplayDelay={3500}
+      navigation={({ setActiveIndex, activeIndex, length }) => (
+        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
+          {new Array(length).fill("").map((_, i) => (
+            <span
+              key={i}
+              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${
+                activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
+              }`}
+              onClick={() => setActiveIndex(i)}
+            />
+          ))}
+        </div>
+      )}
+    >
+      {images.map((src, index) => (
+        <img
+          key={index}
+          src={src}
+          // className="object-none"
+        />
+      ))}
+    </Carousel>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
+const withMT = require("@material-tailwind/react/utils/withMT");
 
-module.exports = {
+module.exports = withMT({
   content: ["./index.html", 
             "./src/**/*/.{jsx,tsx,js,ts}",
             "./src/app.jsx",
@@ -19,5 +20,5 @@ module.exports = {
     },
   },
   plugins: [],
-}
+});
 
