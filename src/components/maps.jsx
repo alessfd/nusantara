f967240cfdcfe5bcae36f5201b772070e5453a15
alessfd <alessandro.fayezdevanno@gmail.com>
2024-05-26 13:19:08 +0700

    final 2

---
+++ src/components/maps.jsx
@@ -0,0 +1,22 @@
+export default async function initMap() {
+  let map;
+  const position = { lat: -0.027165, lng: 109.342853};
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 12,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Pontianak",
+  });
+}
